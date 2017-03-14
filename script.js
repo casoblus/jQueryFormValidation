3a479bf09e32f835800abf77dd59a7daf6185650
Carlos Alberto Sobrino <casobrinolusquinos@gmail.com>
2017-03-14 19:26:41 +0100

    Os que teñan valores asignaralle a clase correctos e os que estén valeiros asignaralle a clase erros.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
     $('#formulario').on('submit', function(){
         $('#formulario [type="text"]').each(function() {
-            if(($(this).val().length == 0)) {
+            if(($(this).val().length === 0)) {
                 console.log($(this).attr('name') + ' está vacío.');
+                $(this).addClass('erros');
+            }
+            else {
+                $(this).addClass('correctos');
             }
         });
         $('#formulario [type="password"]').each(function() {
-            if(($(this).val().length == 0)) {
+            if(($(this).val().length === 0)) {
                 console.log($(this).attr('name') + ' está vacío.');
+                $(this).addClass('erros');
+            }
+            else {
+                $(this).addClass('correctos');
             }
         });
     });
