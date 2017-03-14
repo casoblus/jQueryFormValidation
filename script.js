0f512e9133467d6d8f5bbb304d721a9a0961c51f
Carlos Alberto Sobrino <casobrinolusquinos@gmail.com>
2017-03-14 19:27:47 +0100

    Se hai algún erro deberá cancelar o evento de envío do formulario.

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
     $('#formulario').on('submit', function(){
+        var valido = true;
         $('#formulario [type="text"]').each(function() {
             if(($(this).val().length === 0)) {
                 console.log($(this).attr('name') + ' está vacío.');
+                if($(this).hasClass('correctos')) {
+                    $(this).removeClass('correctos');
+                }
                 $(this).addClass('erros');
+                valido = false;
             }
             else {
+                if($(this).hasClass('erros')) {
+                    $(this).removeClass('erros');
+                }
                 $(this).addClass('correctos');
             }
         });
         $('#formulario [type="password"]').each(function() {
             if(($(this).val().length === 0)) {
                 console.log($(this).attr('name') + ' está vacío.');
+                if($(this).hasClass('correctos')) {
+                    $(this).removeClass('correctos');
+                }
                 $(this).addClass('erros');
+                valido = false;
             }
             else {
+                if($(this).hasClass('erros')) {
+                    $(this).removeClass('erros');
+                }
                 $(this).addClass('correctos');
             }
         });
+        return valido;
     });
 });
